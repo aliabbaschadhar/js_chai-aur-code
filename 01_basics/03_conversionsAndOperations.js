3aa5a8d4d89f77b5ba65689f9ebc9b216050f480
Ali Abbas Chadhar <163995321+aliabbaschadhar@users.noreply.github.com>
2024-03-19 16:40:57 +0000

    conversion and operations

--- 01_basics/03_conversionsAndOperations.js
+++ 01_basics/03_conversionsAndOperations.js
@@ -22,10 +22,45 @@ let boleanIsLoggedIn=Boolean(isLoggedIn);
 
 let age=33;
 
-let stringNumber=String(age);
-console.log(typeof stringNumber); //string
+// let stringNumber=String(age);
+// console.log(typeof stringNumber); //string
 
 let chai=null;
-let stringVar=String(chai);
-console.log(typeof stringVar); //simple console is null but type of null after conversion is string
+// let stringVar=String(chai);
+// console.log(typeof stringVar); //simple console is null but type of null after conversion is string
 
+/************************************Operations************************************/
+
+let value=3;
+let negValue=-value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**2); //2 raise power 2
+// console.log(2**3); //2 raise power 3
+// console.log(2/3);
+// console.log(2%3);//remainder operator
+
+let str1='hello';
+let str2=' ali';
+let str3=str1+str2;
+// console.log(str3);
+
+// console.log('1'+2);//12
+// console.log(1+'2');//12
+// console.log('1'+2+2);//122
+// console.log(1+2+'2');//32
+//If string is at last then first 2 digits will behave like number and if first number is string then all numbers will behave like string and string concatination will happen.
+
+
+console.log(+true);//1
+console.log(+"");//0
+
+let num1 , num2 , num3
+num1=num2=num3=2+2;
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter);
