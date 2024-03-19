ffd7e3a7a88730110493e5125928d3ee0ffef09a
Ali Abbas Chadhar <163995321+aliabbaschadhar@users.noreply.github.com>
2024-03-19 17:39:16 +0000

    Data types questions

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,9 @@
+//There are two types of dataTypes in javascript on basis of how these data types are kept in memory callec premitive and non-primitive datatype.
+//PREMITIVE DATA TYPES
+//7 Types: string , Number , boolean, null , undefined , symbol, bigint
+
+
+//REFRENCE TYPES *****OR***** NON-PREMITIVE DATA TYPES
+//Array , Objects, fuctions
+
+//----->Javascript is a dynamically typed programing language.
