e709e8fe7f06107ce7094805ab8a1e57d8bf1621
Ali Abbas Chadhar <163995321+aliabbaschadhar@users.noreply.github.com>
2024-03-19 11:34:17 +0000

    conversion of data types

---
+++ 01_basics/03_conversionsAndOperations.js
@@ -0,0 +1,31 @@
+let score="33avd";
+
+console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0
+
+let isLoggedIn="ali";
+let boleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(boleanIsLoggedIn);
+
+//1=>true; 0=>false
+//"" =>false
+//"hitesh=>true"
+
+
+let age=33;
+
+let stringNumber=String(age);
+console.log(typeof stringNumber); //string
+
+let chai=null;
+let stringVar=String(chai);
+console.log(typeof stringVar); //simple console is null but type of null after conversion is string
+
