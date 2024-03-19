bc5e1d2d7069fc1ef81a544566cad9ed5b749504
Ali Abbas Chadhar <163995321+aliabbaschadhar@users.noreply.github.com>
2024-03-19 10:35:29 +0000

    container config done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,19 @@
-console.log('ali abbas');
+const accountId = 23323443;
+let accountEmail="aliabbas@gmail.com";
+var accountPassword='34343';
+accountCity="jaipur"; //In javascript we can reserve the memory of variable without writing anything(let , const , var).But it is not a good practice.
+let accountState; // Undefined because we haven't set the value right now.
+
+
+// accountId=33434343;//not allowed
+
+accountEmail="hc@hc.com";
+accountPassword="787877787";
+accountCity="Hafizabad";
+console.log(accountId);
+
+/*
+Prefer not to use var because of issue in block scope and functional scope.
+*/
+
+console.table([accountEmail,accountId, accountCity, accountPassword , accountState]);
