1ba80b6bc570ba96c3babd4cb285aebbc90d6193
Ali Abbas Chadhar <163995321+aliabbaschadhar@users.noreply.github.com>
2024-03-19 11:12:40 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello"); // We are using node js not browser as our compiler
+
+//DATA TYPES IN JAVASCRIPT
+
+//---->Premitive datatypes
+//number => 2 to power 53
+//bigint => if number is too large
+//string => "" ''
+//bolean => true /false
+//null =>standalone value and datatype also . (null means something is empty)
+//undefined => we don't what it is ... we haven't set the value
+//symbol =>unique components mostly used in react
+
+
+//------>Non premitive data types
+
+//object
+
+
+
+let age=8;
+console.log(typeof age); //number
+console.log(typeof null); // object (interview question)
+console.log(typeof undefined); // undefined
