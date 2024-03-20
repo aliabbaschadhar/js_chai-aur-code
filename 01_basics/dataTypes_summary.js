9ee263b2981e7a91ea3af095209c2a289a3d765a
Ali Abbas Chadhar <163995321+aliabbaschadhar@users.noreply.github.com>
2024-03-20 01:50:47 +0000

    Data types summary

--- 01_basics/dataTypes_summary.js
+++ 01_basics/dataTypes_summary.js
@@ -2,8 +2,33 @@
 //PREMITIVE DATA TYPES
 //7 Types: string , Number , boolean, null , undefined , symbol, bigint
 
+const id = Symbol('123');
+const anotherId=Symbol('123');
+console.log(id===anotherId); //false bcz no matter we pass same value to both of the symbol then their return type which also a symbol will be unique.
+
+const bigNumber= 3790480238432009832498n //bigint
+//----->Javascript is a statically typed programing language which means that the datatype of variable is determined by the value they hold at run time and can change throughout the program as we assign different value to them.
+
+
 
 //REFRENCE TYPES *****OR***** NON-PREMITIVE DATA TYPES
+
 //Array , Objects, fuctions
 
-//----->Javascript is a dynamically typed programing language.
+// Most of the primitive data types return object like null ,array and return type of Function is ----> OBJECT FUNCTION
+
+const heros=['ironMan', 'thor', ' captainAmerica'];
+console.log(typeof heros);  //object
+
+const myObj ={
+    name:'ali',
+    age:32,
+}
+
+const myFunction=function(){
+    console.log('Hello world');
+}
+console.log(typeof myFunction);
+
+
+
