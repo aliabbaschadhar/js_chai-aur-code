80e2946d3cb91b908bfd81f2988bca90d5831700
Ali Abbas Chadhar <163995321+aliabbaschadhar@users.noreply.github.com>
2024-03-22 09:54:39 +0000

    truthy and falsy values and  Nullish Coalescing Operator (??)

---
+++ 04_control_flow/03_truthy&falsy.js
@@ -0,0 +1,44 @@
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+// falsy values
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) { //Object.keys() ...converts the object into array
+    console.log("Object is empty");
+}
+
+// Nullish Coalescing Operator (??): null undefined 
+
+//Checks if a value is null or undefined if it is then other value will be returned .
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20 //if there are three values and null is first (null will not be executed) then checked if second value is null or undefined if it is then third one will be executed if it isn't null or undefined.
+
+
+
+console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
